ca15bf5ac53aeb58282b9aa1b6daa3c368a02115
Muhamad Amir <m.amirazmi@hotmail.com>
2025-08-16 21:23:26 +0800

    Basic setup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,36 @@
-import { Link } from '@tanstack/react-router'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { Link } from "@tanstack/react-router";
 
 export default function Header() {
-  return (
-    <header className="p-2 flex gap-2 bg-white text-black justify-between">
-      <nav className="flex flex-row">
-        <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
-        </div>
-      </nav>
-    </header>
-  )
+	return (
+		<header className="p-4 bg-blue-400 text-black ">
+			<div className="container mx-auto flex gap-2 justify-between">
+				<div>Logo</div>
+				<nav className="flex gap-4">
+					<Link to="/">Isu Utama?</Link>
+
+					<DropdownMenu>
+						<DropdownMenuTrigger>Open</DropdownMenuTrigger>
+						<DropdownMenuContent>
+							<DropdownMenuLabel>My Account</DropdownMenuLabel>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem>Profile</DropdownMenuItem>
+							<DropdownMenuItem>Billing</DropdownMenuItem>
+							<DropdownMenuItem>Team</DropdownMenuItem>
+							<DropdownMenuItem>Subscription</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+					<Link to="/">Program Kami?</Link>
+					<Link to="/">Hebahan</Link>
+				</nav>
+			</div>
+		</header>
+	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
