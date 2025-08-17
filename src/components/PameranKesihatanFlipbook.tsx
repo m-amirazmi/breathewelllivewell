6d354e25465e75aedd7a06875f73d65f5f90ef1c
m-amirazmi <amir.azmi@compasia.com>
2025-08-17 16:15:41 +0800

    Add flipbook

--- src/components/PameranKesihatanFlipbook.tsx
+++ src/components/PameranKesihatanFlipbook.tsx
@@ -1,11 +1,14 @@
 export default function PameranKesihatanFlipbook() {
   return (
     <div className="container mx-auto px-4 flex flex-col gap-8 pb-8">
-      <div className="w-full flex gap-8">
-        <img
-          className="w-full h-full object-fill border border-primary"
-          src="https://placehold.co/900x400"
-          alt="https://placehold.co/900x400"
+      <div className="w-full h-screen flex gap-8">
+        <iframe
+          src="https://simplebooklet.com/cegahhariinisihatesokhari"
+          title="Simplebooklet Viewer"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allowFullScreen
         />
       </div>
     </div>
