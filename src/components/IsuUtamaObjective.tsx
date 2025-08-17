bd3868fe859300a91f09a2977dddb8ec0717ae0f
m-amirazmi <amir.azmi@compasia.com>
2025-08-17 14:59:10 +0800

    Rough layout for images

--- src/components/IsuUtamaObjective.tsx
+++ src/components/IsuUtamaObjective.tsx
@@ -2,7 +2,7 @@ import content from '@/content.json'
 
 export default function IsuUtamaOjective() {
   return (
-    <div className="container mx-auto px-4 pt-8 mb-8">
+    <div className="container mx-auto px-4 pt-8 mb-8 md:text-lg">
       <h2 className="text-xl md:text-3xl font-bold mb-4">
         {content['isu_utama.objektif_program']}
       </h2>
