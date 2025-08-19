6a6508284d9f3fb2fb16ce07da3da27b8277a69f
Muhamad Amir <m.amirazmi@hotmail.com>
2025-08-19 23:51:58 +0800

    Fix padding

--- src/components/PameranKesihatanPosters.tsx
+++ src/components/PameranKesihatanPosters.tsx
@@ -4,21 +4,21 @@ export default function PameranKesihatanPosters() {
   return (
     <div className="container mx-auto flex flex-col-reverse gap-4 md:gap-8 py-8">
       <div className="w-full flex flex-wrap">
-        <div className="w-full md:w-1/3 md:px-4 py-4 md:py-0">
+        <div className="w-full md:w-1/3 px-4 py-4 md:py-0">
           <img
             className="w-full rounded-xl"
             src="/images/one.webp"
             alt="/images/one.webp"
           />
         </div>
-        <div className="w-full md:w-1/3 md:px-4 py-4 md:py-0">
+        <div className="w-full md:w-1/3 px-4 py-4 md:py-0">
           <img
             className="w-full rounded-xl"
             src="/images/two.webp"
             alt="/images/two.webp"
           />
         </div>
-        <div className="w-full md:w-1/3 md:px-4 py-4 md:py-0">
+        <div className="w-full md:w-1/3 px-4 py-4 md:py-0">
           <img
             className="w-full rounded-xl"
             src="/images/three.webp"
