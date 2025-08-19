dd46e360625a5386ca067f534b11a979b77b3ef4
Muhamad Amir <m.amirazmi@hotmail.com>
2025-08-19 23:39:47 +0800

    Update the pameran

--- src/components/PameranKesihatanFlipbook.tsx
+++ src/components/PameranKesihatanFlipbook.tsx
@@ -1,7 +1,7 @@
 export default function PameranKesihatanFlipbook() {
   return (
     <div className="container mx-auto px-4 flex flex-col gap-8 pb-8">
-      <div className="w-full h-screen flex gap-8">
+      <div className="w-full h-screen flex gap-8 p-4 border border-accent-content rounded-2xl">
         <iframe
           src="https://simplebooklet.com/cegahhariinisihatesokhari"
           title="Simplebooklet Viewer"
@@ -9,6 +9,7 @@ export default function PameranKesihatanFlipbook() {
           height="100%"
           frameBorder="0"
           allowFullScreen
+          className="rounded-xl"
         />
       </div>
     </div>
--- src/components/PameranKesihatanPosters.tsx
+++ src/components/PameranKesihatanPosters.tsx
@@ -1,35 +1,44 @@
+import video from '@/videos/video.mp4'
+
 export default function PameranKesihatanPosters() {
   return (
     <div className="container mx-auto px-4 flex flex-col gap-8 py-8">
       <div className="w-full flex gap-8">
-        <div className="w-1/3">
+        <div className="w-1/3 p-4 border border-accent-content rounded-2xl">
           <img
-            className="w-full border border-primary"
-            src="https://placehold.co/500x700"
-            alt="https://placehold.co/500x700"
+            className="w-full rounded-xl"
+            src="/images/one.webp"
+            alt="/images/one.webp"
           />
         </div>
-        <div className="w-1/3">
+        <div className="w-1/3 p-4 border border-accent-content rounded-2xl">
           <img
-            className="w-full border border-primary"
-            src="https://placehold.co/500x700"
-            alt="https://placehold.co/500x700"
+            className="w-full rounded-xl"
+            src="/images/two.webp"
+            alt="/images/two.webp"
           />
         </div>
-        <div className="w-1/3">
+        <div className="w-1/3 p-4 border border-accent-content rounded-2xl">
           <img
-            className="w-full border border-primary"
-            src="https://placehold.co/500x700"
-            alt="https://placehold.co/500x700"
+            className="w-full rounded-xl"
+            src="/images/three.webp"
+            alt="/images/three.webp"
           />
         </div>
       </div>
-      <div className="w-full flex-1">
-        <img
-          className="w-full h-full object-fill border border-primary"
-          src="https://placehold.co/900x400"
-          alt="https://placehold.co/900x400"
-        />
+      <div className="w-full p-4 flex-1 border border-accent-content rounded-2xl">
+        <video
+          className="w-full aspect-video rounded-xl"
+          controls
+          src={video}
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
   )
