71b16003537720279d4ffbdc9cf28132e8b41c37
Muhamad Amir <m.amirazmi@hotmail.com>
2025-08-21 23:15:12 +0800

    panelist

--- src/components/ForumPanelist.tsx
+++ src/components/ForumPanelist.tsx
@@ -2,20 +2,22 @@ import content from '@/content.json'
 
 const panelist = [
   {
-    image: 'https://placehold.co/200x200',
-    name: 'Panelist 1',
+    image: '/images/panel1.webp',
+    name: 'Ustaz Muhammad Azam Bin Sofuan',
+    description:
+      'Berkhidmat sebagai Daie Komuniti Yadim Selangor dan Penceramah Bebas',
   },
   {
-    image: 'https://placehold.co/200x200',
-    name: 'Panelist 2',
+    image: '/images/panel2.webp',
+    name: 'Dr. Mohamad Ikhsan Selamat',
+    description:
+      'Pakar dan Pensyarah Kesihatan Awam<br/>Jabatan Kesihatan Awam,<br/>Fakulti Perubatan,<br/>Universiti Teknologi MARA (UiTM)',
   },
   {
-    image: 'https://placehold.co/200x200',
-    name: 'Panelist 3',
-  },
-  {
-    image: 'https://placehold.co/200x200',
-    name: 'Panelist 4',
+    image: '/images/panel3.webp',
+    name: 'Prof. Dr. Siti Munira Yasin',
+    description:
+      'Pakar dan Pensyarah Kesihatan Awam <br/>Jabatan Kesihatan Awam,<br/>Fakulti Perubatan,<br/>Universiti Teknologi MARA (UiTM)',
   },
 ]
 
@@ -25,38 +27,55 @@ export default function ForumPanelist() {
       id="forum"
       className="container w-full mx-auto px-8 flex gap-8 flex-col my-16 scroll-mt-40"
     >
-      <div className="flex flex-col md:flex-row w-full md:gap-8">
+      <div className="flex flex-col mx-auto md:gap-8">
         <h2 className="md:hidden text-center text-xl font-bold mb-4 flex flex-col items-center">
           <span>{content['forum.forum.mobile'].forum}</span>
           <span className="text-2xl">
             {content['forum.forum.mobile'].breathe_well_live_well}
           </span>
         </h2>
-        <div className="md:w-1/3 flex flex-wrap">
+        <div className="flex md:flex-row flex-col flex-wrap items-stretch justify-stretch">
           {panelist.map((panel) => (
-            <div className="w-1/2 p-4">
-              <div>
-                <img
-                  className="w-full h-full rounded-full object-fill border border-primary"
-                  src={panel.image}
-                  alt={panel.name}
-                />
+            <div
+              key={panel.image}
+              className="w-full lg:w-1/3 py-2 md:py-0 md:px-4"
+              style={{ height: 'inherit' }}
+            >
+              <div className="card flex flex-row bg-white card-md shadow-sm h-full">
+                <div className="w-1/2">
+                  <img
+                    className="w-full h-full object-cover object-top rounded-l-2xl aspect-square"
+                    src={panel.image}
+                    alt={panel.name}
+                  />
+                </div>
+                <div className="w-1/2 card-body">
+                  <h2 className="card-title">{panel.name}</h2>
+                  <p dangerouslySetInnerHTML={{ __html: panel.description }} />
+                </div>
               </div>
-              <h4 className="text-lg mt-4 text-center">{panel.name}</h4>
             </div>
           ))}
         </div>
-        <div className="md:w-2/3 flex flex-col gap-8">
-          <div>
+        <div className="md:w-1/2 lg:w-3/5 flex flex-col gap-8 items-center justify-center">
+          <div className="px-4">
             <h2 className="hidden md:block text-4xl font-bold mb-4">
               {content['forum.forum']}
             </h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ullam,
-              minus dignissimos corrupti aspernatur numquam modi neque quibusdam
-              alias, dolorem doloremque. Eum eligendi nobis ab omnis! Qui
-              molestiae deleniti facere!
+            <p className="text-lg">
+              Forum ini akan membincangkan isu penting berkaitan kualiti udara
+              dalaman serta bahaya amalan vaping terhadap kesihatan. Melalui
+              perkongsian ini panel akan mengulas mengenai:
             </p>
+            <ul className="list-disc ml-5">
+              <li>Kesan vape terhadap kesihatan</li>
+              <li>
+                Hukum-hakam berkaitan penggunaan vape menurut perspektif islam
+              </li>
+              <li>
+                Langkah-langkah memastikan kualiti udara dalaman yang baik
+              </li>
+            </ul>
           </div>
         </div>
       </div>
