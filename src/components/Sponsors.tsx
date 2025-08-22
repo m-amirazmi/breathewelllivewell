b9faae440c99668e641f929daf713548d0409bc3
Muhamad Amir <m.amirazmi@hotmail.com>
2025-08-22 12:33:10 +0800

    Add sponsors and change the poster

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,6 +1,6 @@
 import content from '@/content.json'
 
-const images = Array(9)
+const images = Array(10)
   .fill('')
   .map((_, index) => {
     return `/images/collab-${index + 1}.webp`
@@ -15,7 +15,7 @@ export default function Sponsors() {
 
       <div className="flex flex-wrap">
         {images.map((img, index) => (
-          <div className="w-1/3 md:w-1/9 p-2" key={index}>
+          <div className="w-1/4 md:w-1/10 p-2" key={index}>
             <img className="w-full" src={img} alt={`collab-${index}`} />
           </div>
         ))}
